17b5bf38967a061807998e325973e46e86554c24
dleonardo-gomez <41376687+dleonardo-gomez@users.noreply.github.com>
2018-09-26 08:42:00 -0500

    Update derivaditas.py

--- derivaditas.py
+++ derivaditas.py
@@ -9,6 +9,7 @@ def df(x):
 def formula(x,h):
   return (0.5/h) * (-3*f(x)+ 4 * f(x+h) - f(x+(2*h))) + ((h**2)/3)
 
-print("df(2) = ",df(2),end="\n\n")
+print("f'(2) directa = ",df(2),end="\n\n")
 for x in range(1,5):
-  print(formula(2,0.1**x))
+  a = formula(2,0.1**x)
+  print("f'(",2+0.1**x,")",a, "Error: ", abs(a-df(2)))
