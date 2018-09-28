647eaccd8451ac760629533ab9495e876ebdedcd
dleonardo-gomez <41376687+dleonardo-gomez@users.noreply.github.com>
2018-09-28 07:44:32 -0500

    Update normal.py

--- normal.py
+++ normal.py
@@ -1,11 +1,12 @@
 import math
+from decimal import Decimal
 
 def norm(x,sigma, miu):
     val = (1/(sigma * math.sqrt(2*math.pi))) * math.exp((-0.5*((x-miu)/sigma)**2))
     return val
 
 def z(x):
-    return (1/math.sqrt(2*math.pi)) * math.exp(-0.5*((x)**2))
+    return (1/math.sqrt(2*math.pi)) * math.e **(-0.5*((x)**2))
 
 def area(b,h):
     return  b*h
@@ -16,6 +17,27 @@ def reinman(inicio, fin, pSize, sigma, miu):
         aprox += area(pSize,norm(inicio + (pSize/2), sigma, miu))
         inicio += pSize
     return aprox
-    
-print(reinman(-50,0,1,1,0))
-    
+
+def reinmanZ(inicio, fin, pSize):
+    aprox = 0
+    while inicio < fin:
+        aprox += area(pSize,z(inicio + (pSize/2)))
+        inicio += pSize
+    return aprox
+
+#0-3 4 decimales
+
+for i in range(0,10):
+  print("0.000" + str(i),end = " | ")
+
+print("\n")
+x = 0
+while x < 1:
+    cont = 0
+    y = Decimal(reinmanZ(-10,x,0.001))
+   
+    print(round(y,4),end=" | ")
+    cont += 1
+    if(cont%10 == 0):
+      print()
+    x += 0.001
